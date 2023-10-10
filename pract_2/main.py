be84c22e58f749f7f2e34b0526479f928eac8708
Денисов Кирилл Юрьевич <kirill@ex.ru>
2023-10-11 02:30:51 +0300

    Изменения в порядке обработки id и записи в файл

--- pract_2/main.py
+++ pract_2/main.py
@@ -1,17 +1,22 @@
-from typing import List, Any, Dict, Tuple, Optional
+from typing import List, Any, Dict, Tuple, Optional, Set
 from neo4j import GraphDatabase
-
 from itertools import cycle
 
 import csv
-import time
 import requests
 import urllib.parse
 import json
 import yaml
 
-from utils import parse_config
-from pg import logger
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger("vk")
+
+
+def parse_config(filepath:str) -> Dict[str, Any]:
+  with open(filepath,"r") as f:
+    config = yaml.safe_load(f)
+  return config
 
 
 class VkApiAgent:
@@ -43,7 +48,6 @@ class VkApiAgent:
             return []
 
 
-
 class Neo4jAgent:
     def __init__(self,
                  host:str,
@@ -51,13 +55,22 @@ class Neo4jAgent:
                  user:str,
                  password:str,
                  database:str
-                 ):
-        endpoint = f"bolt://{host}:{port}"
-        self.driver = GraphDatabase.driver(endpoint, auth=(user,password))
-        self.database = database
+    ):
+      endpoint = f"bolt://{host}:{port}"
+      self.driver = GraphDatabase.driver(endpoint, auth=(user,password))
+      self.database = database
 
     def close(self):
-        self.driver.close()
+      self.driver.close()
+
+    def _load_person_from_csv(self, tx, filepath):
+      query = f"""load csv from $filepath as line
+      merge (p1:Person {id:line[0]})
+      merge (p2:Person {id:line[1]})
+      merge (p1)-[:IS_FRIENDS_WITH]->(p2);"""
+      params = {"filepath":filepath}
+      tx.run(query, parameters = params)
+
 
     def _add_person_friends(self, tx, root_id:str, leaf_ids:List[str]) -> None:
         query = f"""merge (root:Person {{id:$root_id}}) """
@@ -70,22 +83,26 @@ class Neo4jAgent:
             params[f"leaf_{i}id"]=leaf_id
         tx.run(query, parameters = params)
 
+    def _add_person(self, tx, person_id):
+        result = tx.run("CREATE (p: Person {id: $person_id})", person_id = person_id)
+
     def add_person_friends(self, root_id, leaf_ids:List[str]) -> None:
         with self.driver.session(database = self.database) as session:
             session.execute_write(self._add_person_friends,root_id, leaf_ids)
 
+
+    def load_person_from_csv(self, filepath):
+        with self.driver.session(database = self.database) as session:
+            session.execute_write(self._load_person_from_csv, filepath)
 
     def add_person(self, person_id):
         with self.driver.session(database = self.database) as session:
             session.execute_write(self._add_person, person_id)
             print(f"Person with id = {person_id} was created") 
 
-    def _add_person(self, tx, person_id):
-        result = tx.run("CREATE (p: Person {id: $person_id})", person_id = person_id)
-
-
 
 def create_friends_pairs(root:int, leafs:int) -> List[Tuple[str,str]]:
+  """Create pairs [(x,y0),(x,y1),(x,y2)..] from x:str and ys:List[str] """
   return list(zip(cycle([str(root)]), map(lambda x: str(x),leafs)))
 
 def write_pairs_into_csv(f, pairs):
@@ -102,32 +119,26 @@ def create_friend_graph(config):
     raise SystemExit("Wrong cofnig. Fileds `neo4j`, `input.groupmates` and `vk_api` are expected") from e
   neo4j_db = Neo4jAgent(neo4j["host"], neo4j["port"], neo4j["user"], neo4j["password"], neo4j["database"])
   api_instance = VkApiAgent(vk_api["endpoint"], vk_api["access_token"])
+
+  uid_to_process:Set[int] = set(groupmates)
+  for uid in groupmates: 
+    uid_to_process = uid_to_process.union(api_instance.get_friends(uid))
+
   with open(config["result"]["csv_file"], "w") as f:
-    #a = [process_user(api_instance, friend, f) for friend in [process_user(api_instance, user, f) in groupmates]]
-    #a = [process_user(api_instance, friend, f) for friend in [process_user(api_instance, user, f) for user in groupmates]]
-    a = [[list(map(lambda friend: process_user(api_instance, friend, f), friends)) for friends in [process_user(api_instance, user, f) for user in groupmates]]]
-  print(len(a))
+    for uid in uid_to_process:
+      process_user(api_instance, uid, f)
+
+  neo4j_db.close()
 
 
-
-
-
-def process_user(api_instance, uid:int, f) -> List[int]:
+def process_user(api_instance, uid:int, f) -> List[Tuple[str,str]]:
+  """Find user friends and write pairs `(user_id,friend_id)` to csv file"""
   friends:List[int] = api_instance.get_friends(uid)
   logger.info(f"Count friends of user {uid} = {len(friends)}")
   f_pairs:List[Tuple[str, str]] = create_friends_pairs(uid, friends)
   write_pairs_into_csv(f, f_pairs)
-  logger.info("Write pairs into {f}")
-  return friends
-
-
 
 
 if __name__ == "__main__":
-    config = parse_config("./config.yaml")
-    create_friend_graph(config)
-
-
-
-
-
+  config = parse_config("./config.yaml")
+  create_friend_graph(config)
