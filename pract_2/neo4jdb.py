6d25a103468a8a51e2153d63a427ceca8e5494e2
Денисов Кирилл Юрьевич <kirill@ex.ru>
2023-10-30 02:23:05 +0300

    Изменил шаблонизацию

--- pract_2/neo4jdb.py
+++ pract_2/neo4jdb.py
@@ -21,15 +21,15 @@ class Neo4jAgent:
 
     def add_node_leafs(self, root_label: str, leaf_label: str, relationship: str, root_id:int, leaf_ids:list[int]) -> None:
         def _add_node_leafs(tx, root_label:str, leaf_label:str, relationship:str, root_id:int, leaf_ids:list[int]) -> None:
-            query = "merge (root:$root_label {id:$root_id})"
-            params = {"root_id":root_id, "root_label": root_label}
+            query = f"merge (root: {root_label} {{id: $root_id}})"
+            params = {"root_id":root_id}
             for i, leaf_id  in enumerate(leaf_ids):
                 query += f"""
-                merge (leaf_{i}:$leaf_label {{id:$leaf_{i}id}})
-                merge (root)-[:$relationship]->(leaf_{i})
+                merge (leaf_{i}: {leaf_label} {{id: $leaf_{i}id}})
+                merge (root)-[:{relationship}]->(leaf_{i})
                 """
                 params[f"leaf_{i}id"]=leaf_id
-            tx.run(query, parameters = params, relationship = relationship, leaf_label = leaf_label)
+            tx.run(query, parameters = params)
 
         with self.driver.session(database = self.database) as session:
             session.execute_write(_add_node_leafs, root_label, leaf_label, relationship, root_id, leaf_ids)
@@ -38,7 +38,7 @@ class Neo4jAgent:
     def detach_delete_nodes(self, label:str):
         def _detach_delete_nodes(tx, label:str):
             logger.info(f"Detach delete all `{label}` nodes")
-            result = tx.run("MATCH (n:$label) DETACH DELETE n;", label=label)
+            result = tx.run(f"MATCH (n:{label}) DETACH DELETE n;")
         with self.driver.session(database = self.database) as session:
             session.execute_write(_detach_delete_nodes, label)
 
@@ -48,18 +48,18 @@ class Neo4jAgent:
             src = f"file:///{filepath}"
             print(filepath)
             query  = "load csv from $src as line\n"
-            query += "merge (p1:$label {id:line[0]})\n"
-            query += "merge (p2:$label {id:line[1]})\n"
-            query += "merge (p1)-[:$relationship]->(p2);"
+            query += f"merge (p1:{label} {{id:line[0]}})\n"
+            query += f"merge (p2:{label} {{id:line[1]}})\n"
+            query += f"merge (p1)-[:{relationship}]->(p2);"
             logger.info(f"Load {filepath}")
-            tx.run(query, src = src, label = label, relationship = relationship)
+            tx.run(query, src = src)
 
         with self.driver.session(database = self.database) as session:
             session.execute_write(_load_from_csv, filepath,label, relationship)
 
     def add_node(self, person_id):
         def _add_node(tx, label:str, node_id:int):
-            tx.run("CREATE (p: $label {id: $node_id})", label = label, node_id = node_id)
+            tx.run(f"CREATE (p:{label} {{id: $node_id}})", node_id = node_id)
 
         with self.driver.session(database = self.database) as session:
             session.execute_write(_add_node, person_id)
